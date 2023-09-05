b6050359598d591cb2596db9845b5b1e261f34ee
bitskils <bitskils03@gmail.com>
2023-09-06 00:46:05 +0530

    implementation

--- src/main/java/com/bitskils/websocket/handler/WebSocketHandler.java
+++ src/main/java/com/bitskils/websocket/handler/WebSocketHandler.java
@@ -1,16 +1,14 @@
 package com.bitskils.websocket.handler;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
 @Slf4j
+@Component
 public class WebSocketHandler extends TextWebSocketHandler {
 
     @Override
@@ -18,7 +16,7 @@ public class WebSocketHandler extends TextWebSocketHandler {
         String sessionId = session.getId();
         String remoteAddress = session.getRemoteAddress().toString();
         log.info("WebSocket connection established: Session ID=" + sessionId + ", Remote Address=" + remoteAddress);
-        String welcomeMessage = "Welcome to the WebSocket chat!";
+        String welcomeMessage = "[FROM SERVER] Welcome to the WebSocket chat!";
         session.sendMessage(new TextMessage(welcomeMessage));
     }
 
@@ -26,6 +24,6 @@ public class WebSocketHandler extends TextWebSocketHandler {
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String messageContent = message.getPayload();
         log.info("Received message: " + messageContent);
-        session.sendMessage(new TextMessage("You said: " + messageContent));
+        session.sendMessage(new TextMessage("Server said: " + messageContent));
     }
 }
