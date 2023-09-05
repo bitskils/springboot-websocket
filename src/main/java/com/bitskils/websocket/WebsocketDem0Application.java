a513f7591ffa4de56c316a92113e0e535e531463
bitskils <bitskils03@gmail.com>
2023-09-05 23:55:38 +0530

    initial

---
+++ src/main/java/com/bitskils/websocket/WebsocketDem0Application.java
@@ -0,0 +1,13 @@
+package com.bitskils.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketDem0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketDem0Application.class, args);
+	}
+
+}
